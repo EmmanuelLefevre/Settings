a1b149f3463ebf3fdc2debd5c581b545e6347c49
Emmanuel Lefevre <waiasiaa@protonmail.com>
2023-11-19 02:06:50 +0100

    maj

--- oh-my-posh/.config_powershell/user_profile.ps1
+++ oh-my-posh/.config_powershell/user_profile.ps1
@@ -1,5 +1,3 @@
 oh-my-posh init pwsh --config 'C:\Users\Darka\scoop\apps\oh-my-posh\current\themes\jblab_2021.omp.json' | Invoke-Expression
 
-Set-Location 'C:\Users\Darka'
-
 Import-Module Terminal-Icons
